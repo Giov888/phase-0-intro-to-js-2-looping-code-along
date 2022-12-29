474d5f16a03bd7156d965164d780728c2423094f
Giovanni Goins <goinsg55@gmail.com>
2022-12-29 12:58:15 -0600

    commit tree 1899831ea4fc838423664d568bbe1e97fd487bfc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cards= (["Guadalupe", "Ollie", "Aki"]);
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you ${cards[i]},for the wonderful surprise gift!`);
+    debugger;
+  }
+
+  return cards;
+}
+
+writeCards(cards);
+
+let countup = 0;
+while (countup < 10) {
+  console.log(countup++);
+}
